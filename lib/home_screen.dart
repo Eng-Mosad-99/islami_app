7c71ba715cd1e68048baed0f764f8efa7b84f9c4
Eng_Mosad <mo4841919@gmail.com>
2023-08-28 16:46:38 +0300

    create islami app git init

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'HomeScreen';
-
+ // div
   @override
   Widget build(BuildContext context) {
     return Scaffold(
